116152c8d8d19be8d8974c8034c4da3ba0c6afa2
Mayur Panchal <mayurpanchal002@gmail.com>
2021-11-06 11:33:58 +0530

    feat: :sparkles: Add infinite scroll for images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useCallback, useRef, useState } from 'react';
 import './App.css';
+import usePhotos from './usePhotos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	console.log('App component rendered');
+	const [pageNumber, setPageNumber] = useState(1);
+	const [query, setQuery] = useState('office');
+	const [count, setCount] = useState(0);
+	const { photos, loading, error } = usePhotos(query, pageNumber);
+
+	// Step 2 - Create observer Ref.
+	const observer = useRef();
+	// Step 1 - add a callback function to be called when the node ref is "mounted"
+	// useCallback is used to prevent the callback from being recreated on every render
+	const lastImgRef = useCallback((node) => {
+		// Step 5 - Disconnect old observer from Ref.current
+		if (observer.current) observer.current.disconnect();
+		// Step 3 - Create the new IntersectionObserver and store the obj. in observer.current
+		observer.current = new IntersectionObserver((entries) => {
+			// Step 6 - if intersecting then update the page number
+			if (entries[0].isIntersecting) {
+				setPageNumber((prevPage) => prevPage + 1);
+			}
+		});
+
+		// Step 4 - If node is found, add it to the observer
+		if (node) observer.current.observe(node);
+	}, []);
+
+	return (
+		<div className='App'>
+			<input value={query} onChange={(e) => setQuery(e.target.value)} />
+			<h1>{count}</h1>
+			<button onClick={() => setCount((prev) => prev + 1)}>Count +1</button>
+			{photos.map((photo, index) => {
+				if (photos.length === index + 1) {
+					return (
+						// Step 0 - Add ref of last image for IntersectionObserver
+						<div ref={lastImgRef}>
+							<img
+								src={photo.urls.regular}
+								alt={photo.alt_description}
+								style={{ height: '200px' }}
+							/>
+						</div>
+					);
+				}
+				return (
+					<div>
+						<img
+							src={photo.urls.regular}
+							alt={photo.alt_description}
+							style={{ height: '200px' }}
+						/>
+					</div>
+				);
+			})}
+			{loading && <h1>Loading...</h1>}
+			{error && <h1>Error...</h1>}
+		</div>
+	);
+};
 
 export default App;
